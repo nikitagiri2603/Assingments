a0fce322aa5cb59874bb4d8775f0b6291759f93f
Nikita Giri <nikitagiri04@gmail.com>
2026-06-11 17:47:50 +0530

    Assingment 2

---
+++ Assingment-2/Q1.py
@@ -0,0 +1,12 @@
+a = [10]
+b = [10]
+
+print("---------------")
+print("a = ",a)
+print("b = ",b)
+print("---------------")
+print("Type of a = ",type(a))
+print("Type of b = ",type(b))
+print("---------------")
+print("Id of a = ",id(a))
+print("Id of b = ",id(b))
---
+++ Assingment-2/Q2.py
@@ -0,0 +1,8 @@
+a = 10
+b = 10
+print(id(a))
+print(id(b))
+print(id(a)==id(b))
+print(id(a) is id(b)) # doubt   
+print(a is b)
+
